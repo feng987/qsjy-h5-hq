5cca15053fef84c2b22d4e908e963c23ebe46994
pine <916931772@qq.com>
2016-04-07 12:56:02 +0800

    黄金行情资讯  读取大智慧行情接口

---
+++ js/mui.extend.js
@@ -0,0 +1,32 @@
+/**
+ * MUI扩展
+ * */
+
+//日志输入记录
+var plog = {
+	log:function(v){
+		console.log(pdate.currentFormatDate()+" - "+v);
+	}
+};
+
+//时间对象
+var pdate = {
+	currentFormatDate:function() {
+	    var date = new Date();
+	    var seperator1 = "-";
+	    var seperator2 = ":";
+	    var month = date.getMonth() + 1;
+	    var strDate = date.getDate();
+	    if (month >= 1 && month <= 9) {
+	        month = "0" + month;
+	    }
+	    if (strDate >= 0 && strDate <= 9) {
+	        strDate = "0" + strDate;
+	    }
+	    var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate
+	            + " " + date.getHours() + seperator2 + date.getMinutes()
+	            + seperator2 + date.getSeconds();
+	    return currentdate;
+	}
+}
+
